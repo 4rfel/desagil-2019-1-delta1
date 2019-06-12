89787fd1f863aaf8d519f29ebb4492cae5e6dd7e
4rfel <>
2019-06-12 14:29:01 -0300

    aa

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
@@ -1,10 +1,37 @@
 package br.pro.hashi.ensino.desagil.desafio.model;
 
+import java.util.Random;
+
 public class Target extends Element {
     private final Board board;
+    private Random random;
 
     public Target(int row, int col, Board board) {
         super(row, col);
         this.board = board;
+        this.random = new Random();
+    }
+
+    public void move() {
+
+        int side = random.nextInt(5);
+
+        if (side == 1) {
+            if (row > 0 && !board.isWall(row - 1, col)) {
+                row += -1;
+            }
+        } else if (side == 2) {
+            if (col < board.getNumCols() - 1 && !board.isWall(row, col + 1)) {
+                col += 1;
+            }
+        } else if (side == 3) {
+            if (row < board.getNumRows() - 1 && !board.isWall(row + 1, col)) {
+                row += 1;
+            }
+        } else if (side == 4) {
+            if (col > 0 && !board.isWall(row, col - 1)) {
+                col += -1;
+            }
+        }
     }
 }
